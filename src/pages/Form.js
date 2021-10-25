ee3bbeeba73fb3b1d30440541169ea2dbde409d7
Aaron Cartledge <s3840848@student.rmit.edu.au>
2021-10-25 20:55:09 +1100

    added counter for message length

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -4,7 +4,10 @@ export default function Form() {
     const [fields, setFields] = useState({
         fullName: "", email: "", subject: "", message: ""
     });
-    const [errors, setErrors] = useState({ });
+    const [errors, setErrors] = useState({});
+
+    // Initial State for character count of a message
+    const [count, setCount] = useState(0);
 
 
     const handleSubmit = (event) => {
@@ -13,6 +16,15 @@ export default function Form() {
 
     const handleInputChange = (event) => {
         setFields({ ...fields, [event.target.name]: event.target.value });
+    };
+
+    const handleCount = (event) => {
+        setCount(event.target.value.length);
+    };
+
+    const handleChangeCount = (event) => {
+        handleInputChange(event);
+        handleCount(event);
     };
 
     return (
@@ -32,15 +44,18 @@ export default function Form() {
                 <div className="form-group formLabel my-4">
                     <label htmlFor="subject"><i className="fa fa-tag larger"></i> Subject</label>
                     <input type="text" name="subject" className="form-control borderInput" id="subject"
-                        placeholder="Enter Subject" value={fields.subject} onChange={handleInputChange}></input>
+                        placeholder="Enter Subject" pattern="^[A-Z\s]+$" // Regex for only Capital Letters
+                        title="Subject must be typed in Capital Letters Only" value={fields.subject}
+                        onChange={handleInputChange}></input>
                 </div>
                 <div className="form-group formLabel my-4">
                     <label htmlFor="message"><i className="fa fa-pencil larger"></i> Message (Max Length: 200 Characters)</label>
                     <textarea name="message" className="form-control borderInput" id="message" maxLength="200"
-                    rows="3" placeholder="Enter Your Message" value={fields.message} onChange={handleInputChange}></textarea>
+                        rows="3" placeholder="Enter Your Message" value={fields.message} onChange={handleChangeCount}></textarea>
+                        <h6 className="mt-2 text-danger">{count}/200</h6>
                 </div>
                 <div className="form-group formLabel mb-5">
-                    <button type="submit" className="btn btn-outline-primary btn-lg">Send <i class="fa fa-paper-plane-o"></i></button>
+                    <button type="submit" className="btn btn-outline-primary">Send <i class="fa fa-paper-plane-o"></i></button>
                 </div>
             </form>
         </div>
